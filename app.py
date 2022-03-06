42b1eebe32c0dba61cb09fd579ab7e82e8f59787
TechnoTalksDev <technotalksyt101@gmail.com>
2022-03-05 19:35:03 -0800

    Simplify 404 page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home(): 
     return render_template("index.html")
 
+@app.errorhandler(404)
+def not_found(e):
+    return "<h1>Page not found</h1>"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
