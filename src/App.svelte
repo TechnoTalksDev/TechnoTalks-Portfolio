326d63ef735614a03f83b6cf625a7729a48c2dc0
TechnoTalksDev <technotalksyt101@gmail.com>
2023-05-14 19:27:44 -0700

    Cleaned up
    - Preconnecting to google fonts to load fonts faster
    - Changed favicon
    - Changed accent color for gradient and page
    - Decreased card width for mobile screens

--- src/App.svelte
+++ src/App.svelte
@@ -25,14 +25,14 @@
 </script>
 
 <main>
-  <div id="blob" style="left: {$x}px; top: {$y}px;">
-  </div>
+  <div id="blob" style="left: {$x}px; top: {$y}px;"></div>
+  
   <div class="card glass">
     <h1>Hi!</h1>
-    <h2>technotalks.net is under some construction 🚧<br>This portfolio is being redisigned at the moment. Come back some other time!</h2>
+    <h2>technotalks.net is under some construction 🚧<br>This portfolio is being redesigned at the moment. Come back some other time!</h2>
   </div>
 
   <p>
-    Check out the <a href="https://github.com/TechnoTalksDev/TechnoTalks-Portfolio" target="_blank">repository</a>, for updates!
+    Check out the <a href="https://github.com/TechnoTalksDev/TechnoTalks-Portfolio/tree/wip/construction" target="_blank">repository</a>, for updates!
   </p>
 </main>
