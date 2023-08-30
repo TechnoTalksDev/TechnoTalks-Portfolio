c357002e3a39dda33e7dcbdd02e79ff2d9bd906c
TechnoTalksDev <technotalksyt101@gmail.com>
2023-08-29 23:01:44 -0700

    Blob now continues to about me section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,16 +20,20 @@
 
     document.body.onpointermove = event => {
       const {clientX, clientY} = event;
-
+      console.log(clientX, clientY)
       x.set(clientX-250)
-      y.set(clientY-250)
+      y.set((clientY-250)+window.scrollY)
     }
   }
 </script>
 
+<div id="blur"></div>
+
+<div id="blob" style="left: {$x}px; top: {$y}px;"></div>
+
 <div class = "hero">
-  <div id="blob" style="left: {$x}px; top: {$y}px;"></div>
-  <div id="blur"></div>
+  
+  
   <nav>
     <ul>
       <li><a href="#con">Contact</a></li>
@@ -41,6 +45,7 @@
 
   <h2><strong>Student | Developer</strong></h2>
   <a href="#aboutme"><button>Explore </button></a>
+  <!--
   <div class="wave" id = "aboutme">
     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
         <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
@@ -48,10 +53,13 @@
         <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
     </svg>
   </div>
-
+  -->
 </div>
 
+<!--
 <section id="about" style = "background-image: url({aboutbg});" >
+-->
+<section id="about">
   <div class="side">
     <h1 class="lift" id="about-header">About me</h1>
     <div class="divider"></div>
@@ -68,12 +76,12 @@
         <br/> 
         
       </p>
-      <button>Contact</button>
+      <a href="#con"><button>Contact</button></a>
     </div>  
     <div class="cdivider"></div>
 
   </div>
-
+  <!--
   <div class="wave2">
     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
         <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
@@ -81,7 +89,7 @@
         <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
     </svg>
   </div>
-
+  -->
 </section>
 
 <section id="proj">
@@ -293,7 +301,7 @@ strong {
   -webkit-text-fill-color: transparent;
 }
 
-/* Curve CSS*/
+/* Curve CSS
 .wave {
     position: absolute;
     bottom: 0;
@@ -337,6 +345,9 @@ strong {
     fill: #F1E9DB;
 }
 
+*/
+
+
 /* -------------------- */
 
 .lift {
@@ -365,15 +376,17 @@ strong {
   border-radius: 50%;
   background: linear-gradient(to right, var(--accent-color), lightblue);
   animation: rotate 20s infinite;
-  z-index: -2;
+  z-index: -3;
 }
 
 #blur {
   height: 100%;
   width: 100%;
-  position: absolute;
-  z-index: -1;
+  position: fixed;
+  top: 0;
+  z-index: -2;
   backdrop-filter: blur(10vmax);
+  -webkit-backdrop-filter: blur(10vmax);
 }
 
 button {
