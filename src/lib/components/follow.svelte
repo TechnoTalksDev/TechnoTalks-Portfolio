6c90c673e70efd794890b6c511c58a22596e6a04
TechnoTalksDev <technotalksyt101@gmail.com>
2024-07-17 11:29:50 -0700

    Disable follow if touch

--- src/lib/components/follow.svelte
+++ src/lib/components/follow.svelte
@@ -2,6 +2,9 @@
 	import { browser } from "$app/environment";
 	import { tweened } from 'svelte/motion';
 	import { backOut } from 'svelte/easing';
+	import { fade } from "svelte/transition";
+
+	let enabled = false
 
 	let x = tweened(0, {
 			duration: 2000,
@@ -16,8 +19,17 @@
 	let offset = { x: 0, y: 0 }; 
 
 	function mouseMove(event: { clientX: number; clientY: number; }) {
+		if (!enabled) {
+			enabled = true
+		}
 		x.set(event.clientX - offset.x)
 		y.set(event.clientY - offset.y)
+	}
+
+	function handleTouch() {
+		if (enabled) {
+			enabled = false
+		}
 	}
 
 	$: if (browser) {
@@ -25,14 +37,17 @@
 		offset.x = (blobDimensions?.width ?? 320) / 2
 		offset.y = (blobDimensions?.height ?? 320) / 2
 		document.addEventListener('mousemove', mouseMove)
+		document.addEventListener("touchstart", handleTouch);
 	}
 
 	$: console.log(offset.x, offset.y)
 
 </script>
 
-<div id="blob" class="z-[-1] absolute w-[20rem] h-[17rem] rounded-[50%] bg-gradient-to-br from-primary-500 via-tertiary-500 to-secondary-500 opacity-30 overflow-x-hidden" style="top: {$y}px; left: {$x}px; animation-name: spin; animation-duration: 30s;">
-</div>
+{#if enabled}
+	<div id="blob" class="z-[-1] absolute w-[20rem] h-[17rem] rounded-[50%] bg-gradient-to-br from-primary-500 via-tertiary-500 to-secondary-500 opacity-30 overflow-x-hidden" style="top: {$y}px; left: {$x}px; animation-name: spin; animation-duration: 30s;" transition:fade>
+	</div>
+{/if}
 
 <style>
 	@keyframes spin {
